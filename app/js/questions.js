57479f406924716ef9c108d85e163a21d2f110d0
bianca <bianca.sayan@gmail.com>
2015-02-25 13:59:11 -0500

    you can navigate between questions now

--- app/js/questions.js
+++ app/js/questions.js
@@ -1,6 +1,12 @@
 'use strict';
 
 angular.module('App')
-  .controller('QuestionsCtrl', function ($scope) {
-
+  .controller('QuestionsCtrl', function ($scope, $rootScope, $filter) {
+  	$scope.order = 1;
+  	$scope.next = function () {
+  		$scope.order++;
+  	}
+   	$scope.previous = function () {
+  		$scope.order--;
+  	}
 });
