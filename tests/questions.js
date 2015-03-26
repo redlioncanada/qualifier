c45cccf4cb5b5dae81354de73b8c220177cd578a
bianca <bianca.sayan@gmail.com>
2015-03-26 10:59:38 -0400

    changes to test

--- tests/questions.js
+++ tests/questions.js
@@ -9,7 +9,8 @@ describe('QuestionsCtrl', function() {
         for (var i in obj) {
           count++
         }
-        var ref = Math.floor((Math.random() * count)) 
+        var ref = Math.floor((Math.random() * count))
+        obj[ref].name = ref
         return obj[ref]
     }
 
@@ -21,14 +22,18 @@ describe('QuestionsCtrl', function() {
     it('next with no answer', function() {
       // expect: set $scope.question equal to something random from $scope.questions, then assign no answer to question, then try to go next 
       $scope.question = randomQuestion($scope.questions)
-      $scope.next();
-
+      var currentName = angular.copy($scope.question.name)
+      $scope.next()
+      expect(currentName).toEqual($scope.question.name);
     });
 
-    it('substracts from order', function() {
-      $scope.order = 2; 
-      $scope.previous();
-      expect($scope.order).toEqual(1);
+    it('next with answer', function() {
+      // expect: set $scope.question equal to something random from $scope.questions, then assign no answer to question, then try to go next 
+      $scope.question = randomQuestion($scope.questions)
+      var currentName = angular.copy($scope.question.name)
+      
+      $scope.next()
+      expect(currentName).toNotEqual($scope.question.name)
     });
 
   });
