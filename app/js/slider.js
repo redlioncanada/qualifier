7ca982927831b994697a38359ba993d5b1fb3edc
bianca <bianca.sayan@gmail.com>
2015-05-27 16:04:50 -0400

    slider manually switched answer on change

--- app/js/slider.js
+++ app/js/slider.js
@@ -3,4 +3,13 @@
 angular.module('App')
   .controller('SliderCtrl', function ($scope) {
   	// jslider-value
+  	$scope.setAnswer = function () {
+  		for (var i in $scope.question.show.answers) {
+  			if ($scope.question.show.answers[i].value == $scope.question.show.answer) {
+  				$scope.question.show.answers[i].answer = true
+  			} else {
+  				$scope.question.show.answers[i].answer = false
+  			}
+  		}
+  	}
 });
