17575a72dc40461ee45cdd5b4f7165deaac5a8cd
Kyle L. McGregor <kyle@klmcgregor.com>
2015-06-16 01:54:51 -0400

    Adjust structure and styling for the App Start Screen and Subcategory Screen Styling for the bottom navigation

--- app/js/boxespicture.js
+++ app/js/boxespicture.js
@@ -2,7 +2,7 @@
 console.log("loaded")
 angular.module('App')
   .controller('BoxesPictureCtrl', function ($scope, $rootScope) {
-  		$scope.boxWidth= Math.min(Math.floor(12/$rootScope.questionsData.question.show.answers.length), 1)
+  		// $scope.boxWidth= Math.min(Math.floor(12/$rootScope.questionsData.question.show.answers.length), 1)
   		$scope.clear = function (answers) {
   			for (var a in answers) {
   				answers[a].answer = false
