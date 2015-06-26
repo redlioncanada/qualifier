9647dc90a1abe139f3c67ac08563a29423f5abb1
bianca <bianca.sayan@gmail.com>
2015-06-26 16:49:38 -0400

    new working questions

--- app/js/slider.js
+++ app/js/slider.js
@@ -4,7 +4,9 @@ angular.module('App')
   .controller('SliderCtrl', function ($scope, $rootScope) {
   	// jslider-value
   	$scope.setAnswer = function () {
-  		for (var i in $rootScope.questionsData.show.answers) {
+  		for (var i in $rootScope.questionsData.question.show.answers) {
+        console.log($rootScope.questionsData.question.show.answers[i])
+        console.log($rootScope.questionsData.question.show.answers[i].value == $rootScope.questionsData.question.show.answer,$rootScope.questionsData.question.show.answers[i]['value'],$rootScope.questionsData.question.show.answers[i].value,$rootScope.questionsData.question.show.answer)
   			if ($rootScope.questionsData.question.show.answers[i].value == $rootScope.questionsData.question.show.answer) {
   				$rootScope.questionsData.question.show.answers[i].answer = true
   			} else {
