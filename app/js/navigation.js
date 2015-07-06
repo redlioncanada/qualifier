170cb1b9a96455ef251d0a96af641822337601a3
bianca <bianca.sayan@gmail.com>
2015-07-05 22:55:47 -0400

    handles all navigation

--- app/js/navigation.js
+++ app/js/navigation.js
@@ -28,18 +28,18 @@ angular.module('App')
 	  		var qtype = q.show.type;
 	        for (var ans in q.show.answers ) {
 	          var a = q.show.answers[ans]
-	          if (qtype == "radio" || qtype == "boxes" || qtype == "boxes-picture" || qtype == "boxes-picture-carousel" || qtype == "checkbox" || qtype == "categories") {
-	            if (a.answer == true) {
-	              return a
-	            }
-	          } else if (qtype == "rank") {
+	          if (qtype == "rank") {
 	            if (a.answer == 1) {
 	              return a
 	            }              
-	          } else if (qtype == "slider") {
+	          } else if (qtype == "slider" || qtype == "slider-people") {
 	            if (a.answer == 1) {
 	              return a
 	            }       
+	          } else {
+	            if (a.answer == true) {
+	              return a
+	            }	          	
 	          }
 	        }
 	    }
