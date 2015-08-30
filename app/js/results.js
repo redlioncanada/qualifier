97bbc945547e8e22b3f8f320fbdd121228a536e3
Stephen Poole <stephenwpoole@gmail.com>
2015-08-30 16:46:01 -0400

    Make sure the results columns are the same height

--- app/js/results.js
+++ app/js/results.js
@@ -71,4 +71,25 @@ angular.module('App')
                $scope.setPriceRange()
       }
 
+      //make sure the results columns are the same height
+      $timeout(function() {
+        var padding = [];
+        var parentMargin = [];
+        var maxPadding = 0;
+        $('.result-wrap').each(function(i, el) {
+          var height = $(el).height();
+          var margin = parseInt($(el).parent().css('marginTop'));
+          padding.push(height);
+          parentMargin.push(margin);
+          if (height > maxPadding) maxPadding = height;
+        });
+
+        $('.result-wrap .btn-wrap').each(function(i, el) {
+          if ($(el).hasClass('touched')) return;
+          $(el).addClass('touched');
+          var curPadding = parseInt($(el).css('paddingTop'));
+          curPadding += maxPadding - padding[i] - parentMargin[i];
+          $(el).css('paddingTop', curPadding);
+        });
+      }, 100, false);
 });
