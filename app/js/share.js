2aac75c8b3a021687c237f033b941d454c5997c9
Stephen Poole <Stephen.Poole@redlioncanada.com>
2015-09-01 10:14:08 -0400

    Updated slider asset. Modified button click to respect mousedown and mouseup events. Added click events to share for mobile devices.

--- app/js/share.js
+++ app/js/share.js
@@ -6,18 +6,31 @@ angular.module('App')
       restrict: 'E',
       
       link: function(scope, element, attrs) {
+        $(element).find('.icon-share').on('click', function(e) {
+          popout(e);
+        });
+        $(element).find('.share-popout span').on('click', function(e) {
+          popin(e);
+        });
         element.on('mouseover', function(e) {
-          e.preventDefault();
-          console.log('mouseover');
-          var popout = $(element).find('.share-popout');
-          $(popout).stop(true).animate({'left': -$(popout).width()}, 500);
+          popout(e);
+        });
+        element.on('mouseout', function(e) {
+          popin(e);
         });
 
-        element.on('mouseout', function(e) {
-          console.log('mouseout');
+        function popout(e) {
+          e.preventDefault();
+
+          var popout = $(element).find('.share-popout');
+          $(popout).stop(true).animate({
+            'left': -$(popout).width()
+          }, 500);
+        }
+        function popin(e) {
           var popout = $(element).find('.share-popout');
           $(popout).stop(true).animate({'left': 0}, 500);
-        });
+        }
       }
     }
   });
