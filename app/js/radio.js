1013c0e1bdf9d3fcdaec34a240b8692d8a0689b3
Stephen Poole <Stephen.Poole@redlioncanada.com>
2015-09-28 15:14:39 -0400

    Updated bootstrap dependency to allow for auto-hiding. Tooltip now hides on navigation or answer interaction.

--- app/js/radio.js
+++ app/js/radio.js
@@ -2,6 +2,7 @@
 angular.module('App')
   .controller('RadioCtrl', function ($scope, $rootScope) {
   		$scope.toggle = function (answers, answer) {
+        $rootScope.showTooltip = false;
         if (answer.value == "nothing") {
           $rootScope.controls.questionHasAnswer=false
           for (var a in answers) {
--- app/js/slider-multiple.js
+++ app/js/slider-multiple.js
@@ -4,6 +4,7 @@ angular.module('App')
   .controller('SliderMultipleCtrl', function ($scope, $rootScope) {
   	// jslider-value
   	$scope.setAnswer = function () {
+      $rootScope.showTooltip = false;
       for (var t in $rootScope.questionsData.question.text) {
     		for (var i in $rootScope.questionsData.question.text[t].answers) {
     			if ($rootScope.questionsData.question.text[t].answers[i].value == $rootScope.questionsData.question.text[t].answer) {
