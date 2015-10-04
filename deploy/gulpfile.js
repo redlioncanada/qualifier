4f0cf2bc6950d7a7cc1580311654bdb5be6fbc1c
Stephen Poole <stephenwpoole@gmail.com>
2015-10-04 01:08:35 -0400

    Added version updater to deploy gulpfile

--- deploy/gulpfile.js
+++ deploy/gulpfile.js
@@ -12,30 +12,43 @@ for auth, create a file in the same dir as this one named .ftppass with the stru
 
 var gulp = require('gulp');
 var ftp = require('gulp-sftp');
+var replace = require('gulp-replace');
+var p = require('../package.json');
 
-var base = '/home/wpcstage/mymaytag/test/';
+var basePath = '/home/wpcstage/mymaytag/';
+var versionPath = '/home/wpcstage/mymaytag/latest/';
 var opts = {host: 'wpc-stage.com', port: 22, auth: 'keyMain'}
 
 gulp.task('default', function() {
     doUpload(['config', 'css', 'fonts', 'js', 'views']);
+    updateVersion();
 });
 
 gulp.task('components', function() {
     doUpload(['components', 'img']);
+    updateVersion();
 });
 
 gulp.task('all', function() {
     doUpload(['config', 'css', 'fonts', 'js', 'views', 'components', 'img']);
+    updateVersion();
 });
+
+function updateVersion() {
+    opts.remotePath = versionPath;
+    return gulp.src('version.php')
+        .replace('#VERSION', '\''+p.version+'\'');
+        .pipe(ftp(opts));
+}
 
 function doUpload(src) {
     for (var i in src) {
-        opts.remotePath = base+src[i];
+        opts.remotePath = basePath+p.version+'/'+src[i];
         gulp.src('../build/'+src[i]+'/**')
             .pipe(ftp(opts));
     }
     
-    opts.remotePath = base;
+    opts.remotePath = basePath+p.version+'/';
     return gulp.src('../build/*')
         .pipe(ftp(opts));
 }
---
+++ deploy/version.php
@@ -0,0 +1,3 @@
+<?php
+	$currentVersion = #VERSION;
+?>
