bbed08b0b4c8bd2ab2f2c5d24131f625041b26ab
Stephen Poole <Stephen.Poole@redlioncanada.com>
2015-10-06 17:51:10 -0400

    Added automatic popup of print dialogue on print page

--- app/js/print.js
+++ app/js/print.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('App')
-  .controller('PrintCtrl', function ($scope, $rootScope, $state, $filter, $stateParams) {
+  .controller('PrintCtrl', function ($scope, $rootScope, $state, $timeout, $interval, $filter, $stateParams) {
   	$('footer.maytag-desktop, footer.maytag-mobile, header.header-desktop, header.header-mobile').css('display', 'none');
       if (!!$stateParams.sku) {
       	var a = $filter('filter')($rootScope.appliances, { "sku" : $stateParams.sku});
@@ -13,4 +13,17 @@ angular.module('App')
       } else {
       	$state.go("main.questions");
       }
+
+      var cnt = 0;
+      var interval = 100;
+
+      var print = $interval(function() {
+      	if (parseInt($('.print-page-price span').text().replace('$', '')) > 0) {
+      		$interval.cancel(print);
+      		$timeout(function() {
+      			window.print();
+      		},300);
+      	}
+      	if (++cnt*interval >= 2000) $interval.cancel(print);
+      },interval);
 });
