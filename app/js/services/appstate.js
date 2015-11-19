eb0deb0a77397815f8eddc12819440736da7abb1
Stephen Poole <Stephen.Poole@redlioncanada.com>
2015-11-19 15:25:54 -0500

    Fixed print page to work for all product colors.

--- app/js/services/appstate.js
+++ app/js/services/appstate.js
@@ -119,8 +119,8 @@ appstateModule.factory('$appstate', ['$window', '$state', '$rootScope', 'localSt
 	      return '?' + $base64.encode(JSON.stringify(_enumerateAnswers()));
 	}
 
-	appstate.generatePrintURL = function(sku) {
-	      return '?' + $base64.encode(JSON.stringify({restore:'print',sku:sku}));
+	appstate.generatePrintURL = function(sku,color) {
+	      return '?' + $base64.encode(JSON.stringify({restore:'print',sku:sku,color:color}));
 	}
 
 	appstate.host = function() {
