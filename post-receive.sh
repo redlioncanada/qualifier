22785120024d9b24d13eb5703ad95a7cefb1fa11
Stephen Poole <Stephen.Poole@redlioncanada.com>
2015-12-07 16:14:12 -0500

    Test deploy

--- post-receive.sh
+++ post-receive.sh
@@ -12,6 +12,7 @@ LOGFILE=$GITDIR/post-receive.log
 VERSION="$(git describe --abbrev=0 --tags)"
 VERSIONREGEX = "^v?(\d+\.)?(\d+\.)?(\*|\d+)$"
 
+
 echo -e "Received Push Request at $( date +%F )" >> $LOGFILE
 echo "Old SHA: $oldrev New SHA: $newrev Branch Name: $refname" >> $LOGFILE
 echo "Starting Deploy" >> $LOGFILE
